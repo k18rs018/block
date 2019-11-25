8fc57b25600a80a1af79c654be9c25fb4102e38f
k18rs018 <k18rs018@st.kyusan-u.ac.jp>
2019-11-25 13:59:13 +0900

    ncmbController の初期化処理を実装

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -3,4 +3,16 @@
 // ncmbController.js
 
 var ncmbController = {
+  APPLICATION_KEY: "134968afb07163a8af4430c2bc7b3057a5213e2039043ed5d6dd321783017f61",
+  CLIENT_KEY: "7e5dbee9cfb18fefc4b94e6765a377e2b0277482af1b561e23007a5ad4fdf549",
+
+  ncmb: null,
+  currentUser: null,
+  screenSize: null,
+
+  init: function(screenSize) {
+    var self = this;
+    self.ncmb = new NCMB(self.APPLICATION_KEY,self.CLIENT_KEY);
+    self.screenSize = screenSize;
+  }
 }
