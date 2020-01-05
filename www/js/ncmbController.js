1419e324d95602f8732ade21de56142654f8f5c3
k18rs018 <k18rs018@st.kyusan-u.ac.jp>
2020-01-05 15:04:57 +0900

    ncmbController.js を一部変更

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -3,8 +3,8 @@
 // ncmbController.js
 
 var ncmbController = {
-  APPLICATION_KEY: "134968afb07163a8af4430c2bc7b3057a5213e2039043ed5d6dd321783017f61",
-  CLIENT_KEY: "7e5dbee9cfb18fefc4b94e6765a377e2b0277482af1b561e23007a5ad4fdf549",
+  APPLICATION_KEY: appKey,
+  CLIENT_KEY: clientKey,
 
   ncmb: null,
   currentUser: null,
